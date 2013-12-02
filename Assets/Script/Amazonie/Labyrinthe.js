76aaae0f7e95e181dfeacb7df7739b3c74f0d2e8
BabacarPlusPlus <stephanepez@gmail.com>
2013-12-02 19:14:17 +0100

    modif

--- Assets/Script/Amazonie/Labyrinthe.js
+++ Assets/Script/Amazonie/Labyrinthe.js
@@ -3,8 +3,9 @@
 //var t : GUIText;
 //t.guiText.text = "Tu as 10 min pour trouver la sortie,\n de plus il te faudra trouver les 5 objets antique,\nbon courage jeune aventurier !";
 
-private var textfield:GUIText;
+
 // the textfield to update the time to 
+
 
  
 // time variables 
@@ -21,7 +22,7 @@ function Start () {
 
 function Update () 
 {
-	
+	//object = GameObject.Find("GUIPanel/TxtTimer").GetComponent(GUIText);
 }
 
 
@@ -41,35 +42,41 @@ function OnMouseDown()
 function OnTriggerEnter( other : Collider ) 
 {  
  // retrieve the GUIText Component and set the text 
- textfield = GameObject.Find("GUIPanel/TxtTimer").GetComponent(GUIText); 
+ GameVariables.textfield2 = GameObject.Find("GUIPanel/TxtTimer").GetComponent(GUIText); 
+ GameVariables.textfield = GameObject.Find("GUIPanel/TxtObjets").GetComponent(GUIText); 
+ GameVariables.textfield.text = "Objets Trouvés 0/5";
  
- UpdateTimerText(); 
+ 
+ 	UpdateTimerText(); 
+ 	GameVariables.textfield2.text = "Trouvez la sortie du labyrinthe "; 
  
  // start the timer ticking 
- TimerTick(); 
+	 TimerTick(); 
 } 
  
 function UpdateTimerText() 
 { 
  // update the textfield 
- textfield.text = "Temps restant : " + currentTime.ToString()+ " sec "; 
+ GameVariables.textfield2.text = "Temps restant : " + currentTime.ToString()+ " sec "; 
 } 
  
 function TimerTick() 
 { 
  // while there are seconds left 
- while(currentTime > 0) 
+ var win = GameVariables.RDCwin;
+ if(win) currentTime =0;
+ while((currentTime > 0) && (!win)) 
  { 
- // wait for 1 second 
- yield WaitForSeconds(1); 
+ 	// wait for 1 second 
+	 yield WaitForSeconds(1); 
  
- // reduce the time 
- currentTime--; 
+	 // reduce the time 
+	 currentTime--; 
  
- UpdateTimerText(); 
+	 UpdateTimerText(); 
  } 
  
  // game over and restart 
- Application.LoadLevel("Intro"); 
+ Application.LoadLevel("Temple Aztèque"); 
  
 } 
---
+++ Assets/Script/Amazonie/objetRDC.js
@@ -0,0 +1,101 @@
+﻿#pragma strict
+
+
+
+
+
+public function getRDCwin()
+
+{ return GameVariables.RDCwin; }
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function AddBleu () {
+	GameVariables.objetbleu=1;
+}
+
+function AddJaune () {
+	GameVariables.objetjaune=1;
+}
+
+function AddRouge () {
+	GameVariables.objetrouge=1;
+}
+
+function AddVert () {
+	GameVariables.objetvert=1;
+}
+
+function AddBlanc () {
+	GameVariables.objetblanc=1;
+}
+
+
+function OnTriggerEnter( other : Collider ) {
+
+	var GlobalsScenes : GameObject;
+	var textfield:GUIText = GameObject.Find("GUIPanel/TxtObjets").GetComponent(GUIText);
+	var textfield2:GUIText = GameObject.Find("GUIPanel/TxtTimer").GetComponent(GUIText); 
+
+	if(other.gameObject.CompareTag("Player")){
+		Destroy(gameObject);
+		
+		var obj: GameObject = GameObject.Find("objet-bleu");
+		var obj1: GameObject = GameObject.Find("objet-rouge");
+		var obj2: GameObject = GameObject.Find("objet-vert");
+		var obj3: GameObject = GameObject.Find("objet-blanc");
+		var obj4: GameObject = GameObject.Find("objet-jaune");		
+		
+		
+		
+		
+		if (gameObject.Equals(obj))
+		{
+			AddBleu();
+			GameVariables.nombreObjTrouveRdc++;
+			//Debug.Log ("okkkkkkkkk",obj);
+			
+		}
+		if (gameObject.Equals(obj1))
+		{
+			AddRouge();
+			GameVariables.nombreObjTrouveRdc++;
+		}
+		if (gameObject.Equals(obj2))
+		{
+			AddVert();
+			GameVariables.nombreObjTrouveRdc++;
+		}
+		if (gameObject.Equals(obj3))
+		{
+			AddBlanc();
+			GameVariables.nombreObjTrouveRdc++;
+		}
+		if (gameObject.Equals(obj4))
+		{
+			AddJaune();
+			GameVariables.nombreObjTrouveRdc++;
+		}
+		
+		textfield.text = "Objets trouvé : "+ GameVariables.nombreObjTrouveRdc+" /5 ";
+		
+		
+		if((GameVariables.objetbleu) && (GameVariables.objetjaune) && (GameVariables.objetrouge) && (GameVariables.objetvert) && (GameVariables.objetblanc))
+		{
+			textfield2.text = ""; 
+			textfield.text = "bravo !"; 
+			GameVariables.RDCwin=1;
+		}
+		
+		
+		if( audio ){
+			audio.Play();
+		}
+	}
+}
