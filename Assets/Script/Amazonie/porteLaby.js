79e2e85b1c975e1f268f8492aa31b0e70384d4d0
BabacarPlusPlus <stephanepez@gmail.com>
2013-12-08 01:59:57 +0100

    fonctionnalité  "rdc + etage 1" opérationnels

---
+++ Assets/Script/Amazonie/porteLaby.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+var z;
+var colliderTriged =false;
+var animationEffectuee=false;
+
+
+
+
+function Start () {
+	
+}
+
+function Update () {
+
+	
+	var porte: GameObject = GameObject.Find("porteRdc");
+	if((GameVariables.RDCwinLvl1==1)&&(colliderTriged==true)&&(animationEffectuee==false) )	
+	{
+			var	z =	Time.deltaTime	*	(0.5);
+			porte.transform.Translate(0,0,-z);	
+			GameVariables.porteOpen=1;
+			var r   = porte.transform.position.x.ToString();
+			Debug.Log("ouff="+r);
+			
+			if(porte.transform.position.x < 1097.8)
+			{
+				animationEffectuee=true;//empeche que la porte ne se réouvrir
+				
+			}
+	}	
+}
+
+
+function OnTriggerEnter( other : Collider ) 
+{
+	//ouverture de la porte
+	if(GameVariables.RDCwinLvl1==1)
+	{
+		colliderTriged=true;
+		var textfield:GUIText = GameObject.Find("GUIPanel/TxtObjets").GetComponent(GUIText);
+		textfield.text = "La porte s'ouvre"; 
+	}
+	
+		
+}
