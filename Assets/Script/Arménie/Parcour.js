f421f1c863b997e80329d489015b7014927be7dc
Bourhane <bossdbz@hotmail.com>
2013-12-10 00:56:35 +0100

    Début scene bonus arménie !

--- Assets/Script/Arménie/Parcour.js
+++ Assets/Script/Arménie/Parcour.js
@@ -7,9 +7,11 @@
 private var nomParcour : String;
 var numParcour : int;
 var nombrePoint : int;
+var chrono : GUITexture; 
 private var tresorCur : GameObject; 
 private var declencher : boolean;
 private var i : int = 1;
+
 
 function update(){
 	if ( declencher ){
@@ -50,78 +52,13 @@ function OnTriggerEnter( other : Collider ) {
 		enable(tresorCur);
 		declencher = true;
 		
-		//tresorCur.active = true;
-		/*if(tresorCur) Debug.Log("trouver"); else Debug.Log("pas trouver");
-		Debug.Log(tresorCur.name);
-		
-		var renderers = tresorCur.GetComponentsInChildren(Renderer);
 	
-	for (var r : Renderer in renderers) {
-    	r.enabled = true;
-	}//*/
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*Minuteur();
-		world.SendMessage("AddTresor");
-		if( audio ){
-			audio.Play();
-		}
-	
-		nomObjectParent = transform.gameObject.transform.parent.gameObject.name;
-		Debug.Log(nomObjectParent);
-		pere = transform.gameObject.name;
-		tresor = transform.parent.gameObject.GetComponentsInChildren(typeof(Transform));
-		//tresor =  GameObject.Find(nomObjectParent+"/Tresor");
-		for(var obj : Transform in tresor){
-			Debug.Log(obj.gameObject.name);
-			//obj.gameObject.r	enderer.enabled = true;
-			//obj.gameObject.collider.enabled = true;
-		}//*/
-			
 	}
 }
 
 
 
 
-/*function Minuteur()
-{
-	// retrieve the GUIText Component and set the text
-	textfield = GameObject.Find("GUIPanel/TxtTimer").GetComponent(GUIText);
-	UpdateTimerText();
-	// start the timer ticking
-	TimerTick();
-}
 
-function UpdateTimerText()
-{
-	// update the textfield
-	textfield.text = "time:" + currentTime.ToString();
-}
-
-function TimerTick()
-{
-	// while there are seconds left
-	while(currentTime > 0)
-	{
-		// wait for 1 second
-		yield WaitForSeconds(1);
-		// reduce the time
-		currentTime--;
-		UpdateTimerText();
-	}
-	
-	// game over and restart
-	Application.LoadLevel("Intro");
-}
 //*/
 
