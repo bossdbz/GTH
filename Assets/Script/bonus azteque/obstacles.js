bdc577f31f6e3f9423924485d211f7ad564fbf3d
BabacarPlusPlus <stephanepez@gmail.com>
2013-12-15 05:17:42 +0100

    ajouts

---
+++ Assets/Script/bonus azteque/obstacles.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+
+
+function Start () {
+
+}
+
+function Update () {
+		//var vitesse = Random.Range(0,30);
+		this.transform.Translate(0,0,0);
+		var avion = GameObject.Find("avion");
+		this.transform.Rotate(22*Time.deltaTime,25,15);
+		if (this.transform.position.x - avion.transform.position.x < -5)
+	 	{
+	 		var i = this.transform.position.x - avion.transform.position.x;
+	 		Debug.Log(" comparR : "+ i );
+	 		Destroy(this);
+	 		Destroy(gameObject);
+	 	}
+}
+
+function OnTriggerEnter( other : Collider ) {
+
+	AvionVariables.nbDetruits = AvionVariables.nbDetruits + 50;
+	if( other.name == "avion" ) 
+	{
+		AvionVariables.vie = AvionVariables.vie - 1;
+	}
+	else Debug.Log(" marche pas");
+	Destroy(gameObject);
+
+}
---
+++ Assets/Script/bonus azteque/temps.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+public var textfield:GUIText;
+public var textfield2:GUIText;
+public var textfield3:GUIText;
+public var textfield4:GUIText;
+
+public static class AvionVariables 
+{ 
+	//le rdc
+	public var distance = 0;
+	public var vie = 5;
+	public var nbDetruits = 0;
+	public var munitions = 200; 
+}
+
+function Start () {
+
+}
+
+function Update () {
+	distance();
+	vies();
+	score();
+	munitions();
+}
+
+function distance()
+{
+	yield WaitForSeconds(1); 
+	AvionVariables.distance = AvionVariables.distance + 1;
+	textfield = GameObject.Find("distance").GetComponent(GUIText); 
+	textfield.text = "Distance : " + AvionVariables.distance+ "m ";
+	
+}
+
+
+function vies()
+{
+	textfield2 = GameObject.Find("vies").GetComponent(GUIText); 
+	textfield2.text = "" + AvionVariables.vie;  
+}
+
+function score()
+{
+	textfield3 = GameObject.Find("score").GetComponent(GUIText); 
+	textfield3.text = ""+ AvionVariables.nbDetruits;
+}
+
+function munitions()
+{
+	textfield4 = GameObject.Find("munitions").GetComponent(GUIText); 
+	textfield4.text = "" + AvionVariables.munitions;
+}
