5e233319a9b5efa3ddbe35448a46a4c4068782bb
BabacarPlusPlus <stephanepez@gmail.com>
2013-12-18 00:52:11 +0100

    niveau bonus fini, manque que les affichages

---
+++ Assets/Script/bonus azteque/explosion.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	    var avion = GameObject.Find("avion");
+		
+		if (this.transform.position.x - avion.transform.position.x < -1)
+	 	{
+	 		Destroy(this);
+	 		Destroy(gameObject);
+	 	}
+}
