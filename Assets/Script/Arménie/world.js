3483f831e6919629128c7c8a4ec69a7f209dcf2a
BabacarPlusPlus <stephanepez@gmail.com>
2013-12-20 04:17:14 +0100

    add

--- Assets/Script/Arménie/world.js
+++ Assets/Script/Arménie/world.js
@@ -29,7 +29,7 @@ function Awake(){
 	//nbrTresors = GameObject.FindGameObjectsWithTag("Tresor").Length;
 	
 	sceneName = EditorApplication.currentScene; 
-<<<<<<< HEAD
+
 
 	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
 
@@ -38,10 +38,10 @@ function Awake(){
 	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
 
 	
-=======
+
 	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
 	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
->>>>>>> 5cfffb93f5626f09c28d4b3c244bd745dd154a8c
+
 	textTresor = GameObject.Find("InterfaceJeux/TextTresor").GetComponent(GUIText);
 
 	textPiece = GameObject.Find("InterfaceJeux/TextPiece").GetComponent(GUIText);
@@ -62,6 +62,7 @@ function Awake(){
 	OnGUI();
 }
 }
+}}
 
 function OnGUI() {
 	
