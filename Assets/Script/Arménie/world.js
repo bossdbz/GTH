65ab900a75fd116be03592a35a1112a0ea68eab1
Bourhane <bossdbz@hotmail.com>
2013-12-29 20:14:12 +0100

    no

--- Assets/Script/Arménie/world.js
+++ Assets/Script/Arménie/world.js
@@ -27,39 +27,29 @@ function AddTresorBonus () {
 
 function Awake(){
 	//nbrTresors = GameObject.FindGameObjectsWithTag("Tresor").Length;
-<<<<<<< HEAD
+
 	sceneName = Application.loadedLevel; 
 	
-=======
-	sceneName = EditorApplication.currentScene; 
-<<<<<<< HEAD
-
-
-
+	
 	
 
-=======
-	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
-	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
->>>>>>> 56179bc8e719da66e25bd651f85405aa0150162a
->>>>>>> 8681734b956a5a0e3a125e32d8c6a32837e94451
 	textTresor = GameObject.Find("InterfaceJeux/TextTresor").GetComponent(GUIText);
 
 	textPiece = GameObject.Find("InterfaceJeux/TextPiece").GetComponent(GUIText);
 	
 	
 	
-<<<<<<< HEAD
-	
-	
-	
-	if (sceneName == 7){
-=======
 
 	
 	
-	if (sceneName == "Assets/Scene/Bonus Syrie.unity"){
->>>>>>> 8681734b956a5a0e3a125e32d8c6a32837e94451
+	
+	
+
+
+	
+	
+	if (sceneName == 7){
+
 	textTresorBonus = GameObject.Find("InterfaceJeux/TextTresorBonus").GetComponent(GUIText);
 	}
 	
@@ -68,32 +58,27 @@ function Awake(){
 	
 	
 	OnGUI();
-<<<<<<< HEAD
 
-=======
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 8681734b956a5a0e3a125e32d8c6a32837e94451
-}
->>>>>>> 56179bc8e719da66e25bd651f85405aa0150162a
+
+
 
 function OnGUI() {
 	
 	
 	
-	if (sceneName != "Assets/Scene/Bonus Syrie.unity"){
+	
 	textTresor.text = "Trésors : " + GameVariable.nbrTresor + "";
 	
 	textPiece.text = "Pièces : " + GameVariable.nbrPiece + "";
+
 	
-	}
 	
 	
 	textVie.text = "Vies : " + GameVariable.nbrVie + "";
 	
-	if (sceneName == "Assets/Scene/Bonus Syrie.unity") {
+	if (sceneName == 7) {
 	textTresorBonus.text = "Trésors Bonus : " + GameVariable.nbrTresorBonus + " /10";
 	}
 }
