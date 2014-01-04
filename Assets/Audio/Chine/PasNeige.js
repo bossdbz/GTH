26d3204514f8dd1a749ae51cad9a5e75d3110d7f
Nicolas34 <nicolashergle@gmail.com>
2014-01-04 15:25:41 +0100

    Test son de marche neige, problème test script à cause d'erreur sur un autre script qui n'est pas à moi donc j'ai pas touché, j'espère que de refaire un update corrigera ça

---
+++ Assets/Audio/Chine/PasNeige.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var snow : AudioClip;
+var character : CharacterController;
+
+function Update () {
+
+if (Input.GetKeyDown ("Z") && character.isGrounded)
+{
+    audio.clip = snow;
+ 	audio.Play();
+}
+
+if (Input.GetKeyUp ("w"))
+{
+    audio.Stop();
+}
+
+}
