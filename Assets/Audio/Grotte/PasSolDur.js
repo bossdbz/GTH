4b5b1219acb6c262519103c1cd635ac34a3952dc
Nicolas34 <nicolashergle@gmail.com>
2014-01-07 14:45:05 +0100

    Ajout son piece, bruit de pas laby/grotte et bruit mort

---
+++ Assets/Audio/Grotte/PasSolDur.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var soldur : AudioClip;
+var character : CharacterController;
+
+function Update () {
+
+if (( Input.GetButtonDown( "Horizontal" ) || Input.GetButtonDown( "Vertical" ) ) && !audio.isPlaying && character.isGrounded)
+{
+    audio.clip = soldur;
+    //audio.volume = 0.1f;
+ 	audio.Play();
+}
+
+if (!Input.GetButton( "Horizontal" ) && !Input.GetButton( "Vertical" ) && audio.isPlaying)
+{
+    audio.Stop();
+}
+
+if (!character.isGrounded)
+{
+	audio.mute = true;
+}
+else
+{
+	audio.mute = false;
+}
+}
--- Assets/Packages/Laby/PerdreVie.js
+++ Assets/Packages/Laby/PerdreVie.js
@@ -2,13 +2,17 @@
 
 public var monde:GameObject; 
 var charMotor : CharacterMotor;
+var crimort : AudioClip;
 
 function OnTriggerEnter (other : Collider)
 {
 	if (other.gameObject.tag == "Player") 
 	{
+		
+		AudioSource.PlayClipAtPoint(crimort, other.transform.position);
 		charMotor = other.GetComponent(CharacterMotor);
 		charMotor.enabled = false;
+		
 		yield WaitForSeconds(3);
 		charMotor.enabled = true;
 		monde = GameObject.Find("World");
